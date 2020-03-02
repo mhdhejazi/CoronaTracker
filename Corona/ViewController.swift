6cb36826b72515856290712d06ae2cdcb1735397
MhdHejazi <mohammadhi@gmail.com>
2020-03-02 12:14:36 +0300

    Initial Commit

---
+++ Corona/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Corona
+//
+//  Created by Mohammad on 3/2/20.
+//  Copyright © 2020 Samabox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
