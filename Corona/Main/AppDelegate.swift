7082749a795713b40595d5326d6cd2afef6d9363
MhdHejazi <mohammadhi@gmail.com>
2020-03-06 02:24:24 +0300

    Added Crashlytics.

--- Corona/Main/AppDelegate.swift
+++ Corona/Main/AppDelegate.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
+import Firebase
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
+		FirebaseApp.configure()
 		return true
 	}
 
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '12.0'
+
+target 'Corona Tracker' do
+  use_frameworks!
+
+  pod 'Firebase/Analytics'
+	pod 'Firebase/Crashlytics'
+end
