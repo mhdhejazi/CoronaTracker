035ca565669e837595b588dfb03001909b24c50f
Piotr Ożóg <piotr@pbeodev.com>
2020-03-15 17:21:30 +0100

    update target name

--- CoronaTrackerData/Package.swift
+++ CoronaTrackerData/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/saoudrizwan/Disk.git", .upToNextMinor(from: "0.6.4")),
-        .package(url: "https://github.com/yaslab/CSV.swift.git", .upToNextMinor(from: "2.4.2"))
+        .package(url: "https://github.com/yaslab/CSV.swift.git", .upToNextMinor(from: "2.4.3"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
