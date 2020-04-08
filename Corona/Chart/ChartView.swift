fc5288144dec8461205e8a1fa55d1b2f90e8a752
MhdHejazi <mohammadhi@gmail.com>
2020-04-08 11:08:54 +0300

    Allow dismissing chart screen with a swipe.

--- Corona/Chart/ChartView.swift
+++ Corona/Chart/ChartView.swift
@@ -245,7 +245,39 @@ class ChartView<C: ChartViewBase>: UIView, RegionChartView {
 	}
 }
 
-class BaseBarChartView: ChartView<BarChartView> {
+extension ChartViewBase {
+	func shouldAllowPanGesture(for gestureRecognizer: UIGestureRecognizer) -> Bool {
+		#if targetEnvironment(macCatalyst)
+		return super.gestureRecognizerShouldBegin(gestureRecognizer)
+		#else
+		guard let panGestureRecognizer = gestureRecognizer as? UIPanGestureRecognizer else {
+			return super.gestureRecognizerShouldBegin(gestureRecognizer)
+		}
+
+		let velocity = panGestureRecognizer.velocity(in: self)
+		let isHorizontalPan = abs(velocity.x) >= abs(velocity.y)
+		if panGestureRecognizer.view == self {
+			return isHorizontalPan || abs(velocity.y) < 300 /// For our recognizer, allow horizontal & slow vertical movements
+		} else {
+			return !isHorizontalPan /// For others, allow only vertical (to dismiss dialog)
+		}
+		#endif
+	}
+}
+
+class BarChartViewWithHorizontalPanning: BarChartView {
+	override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+		shouldAllowPanGesture(for: gestureRecognizer)
+	}
+}
+
+class LineChartViewWithHorizontalPanning: LineChartView {
+	override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+		shouldAllowPanGesture(for: gestureRecognizer)
+	}
+}
+
+class BaseBarChartView: ChartView<BarChartViewWithHorizontalPanning> {
 	override var interactive: Bool {
 		didSet {
 			chartView.pinchZoomEnabled = interactive
@@ -272,7 +304,7 @@ class BaseBarChartView: ChartView<BarChartView> {
 	}
 }
 
-class BaseLineChartView: ChartView<LineChartView> {
+class BaseLineChartView: ChartView<LineChartViewWithHorizontalPanning> {
 	override var interactive: Bool {
 		didSet {
 			chartView.pinchZoomEnabled = interactive
