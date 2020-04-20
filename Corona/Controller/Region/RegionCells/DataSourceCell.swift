533ba70c88dae21ee7d800fea52a79cd698eb9ad
Mhd Hejazi <mohammadhi@gmail.com>
2020-04-20 23:42:52 +0300

    Update data source info.

--- Corona/Controller/Region/RegionCells/DataSourceCell.swift
+++ Corona/Controller/Region/RegionCells/DataSourceCell.swift
@@ -13,13 +13,27 @@ class DataSourceCell: RegionDataCell {
 	override func awakeFromNib() {
 		super.awakeFromNib()
 
-		labelSource.text = L10n.Data.source("CSSE at Johns Hopkins University")
+		labelSource.text = L10n.Data.source("")
 	}
 
 	// MARK: - Actions
 
-	@IBAction private func buttonInfoTapped(_ sender: Any) {
+	@IBAction private func buttonJHUTapped(_ sender: Any) {
 		let url = URL(string: "https://arcgis.com/apps/opsdashboard/index.html#/85320e2ea5424dfaaa75ae62e5c06e61")!
+		let safariController = SFSafariViewController(url: url)
+		safariController.modalPresentationStyle = .pageSheet
+		App.topViewController.present(safariController, animated: true)
+	}
+
+	@IBAction private func buttonBingTapped(_ sender: Any) {
+		let url = URL(string: "https://bing.com/covid/")!
+		let safariController = SFSafariViewController(url: url)
+		safariController.modalPresentationStyle = .pageSheet
+		App.topViewController.present(safariController, animated: true)
+	}
+
+	@IBAction private func buttonRKITapped(_ sender: Any) {
+		let url = URL(string: "https://experience.arcgis.com/experience/478220a4c454480e823b17327b2bf1d4/")!
 		let safariController = SFSafariViewController(url: url)
 		safariController.modalPresentationStyle = .pageSheet
 		App.topViewController.present(safariController, animated: true)
