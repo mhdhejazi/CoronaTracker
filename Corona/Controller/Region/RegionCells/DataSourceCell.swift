a709b12215be87499c69af21e8cb12fa1577571c
Patrick <patrick@helmsdeep.at>
2020-04-28 12:46:01 +0200

    Added BMSGPK as data source

--- Corona/Controller/Region/RegionCells/DataSourceCell.swift
+++ Corona/Controller/Region/RegionCells/DataSourceCell.swift
@@ -16,26 +16,31 @@ class DataSourceCell: RegionDataCell {
 		labelSource.text = L10n.Data.source("")
 	}
 
+	private func presentSafariViewController(with url: URL) {
+		let safariController = SFSafariViewController(url: url)
+		safariController.modalPresentationStyle = .pageSheet
+		App.topViewController.present(safariController, animated: true)
+	}
+
 	// MARK: - Actions
 
 	@IBAction private func buttonJHUTapped(_ sender: Any) {
 		let url = URL(string: "https://arcgis.com/apps/opsdashboard/index.html#/85320e2ea5424dfaaa75ae62e5c06e61")!
-		let safariController = SFSafariViewController(url: url)
-		safariController.modalPresentationStyle = .pageSheet
-		App.topViewController.present(safariController, animated: true)
+		presentSafariViewController(with: url)
 	}
 
 	@IBAction private func buttonBingTapped(_ sender: Any) {
 		let url = URL(string: "https://bing.com/covid/")!
-		let safariController = SFSafariViewController(url: url)
-		safariController.modalPresentationStyle = .pageSheet
-		App.topViewController.present(safariController, animated: true)
+		presentSafariViewController(with: url)
 	}
 
 	@IBAction private func buttonRKITapped(_ sender: Any) {
 		let url = URL(string: "https://experience.arcgis.com/experience/478220a4c454480e823b17327b2bf1d4/")!
-		let safariController = SFSafariViewController(url: url)
-		safariController.modalPresentationStyle = .pageSheet
-		App.topViewController.present(safariController, animated: true)
+		presentSafariViewController(with: url)
+	}
+
+	@IBAction func buttonBMSGPKTapped(_ sender: Any) {
+		let url = URL(string: "https://experience.arcgis.com/experience/fb603473e1f74f0bbae48155ff238565/")!
+		presentSafariViewController(with: url)
 	}
 }
